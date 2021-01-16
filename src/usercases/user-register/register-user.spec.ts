e25c89d91c7d12dd99eb64034a139b302eb28aab
aamgoulart <aamgoulart@outlook.com>
2021-01-16 18:07:53 -0300

    week5

--- src/usercases/user-register/register-user.spec.ts
+++ src/usercases/user-register/register-user.spec.ts
@@ -1,6 +1,7 @@
 import { UserRepository } from "../ports/user-repository";
 import { InMemoryUserRepository } from "./repository/in-memory-user-repository";
 import { UserData } from "../../entities/user-data";
+import { RegisterUserOnMailingList } from "./register-on-mailist";
 
 describe('Register user on mailing lis use case', () => {
     test("should add user with complete data to mailing list", () => {
